753773e9bb7e02fc57ba1bd2f5e4f1df56fb6270
shiro9ane <104496306+shiro9ane@users.noreply.github.com>
2022-04-27 19:25:28 +0800

    Update gradeExer_2.c
    Jason: I just changed a line of code.

--- gradeExer_2.c
+++ gradeExer_2.c
@@ -186,7 +186,7 @@ main()
    
    /* Provide the appropriate function calls 
       based on the given printf() below: */
-   printf("Number of 4.0: %d", count4(aGrades, 20));
+   printf("Number of 4.0: %d", count4(aGrades, MAX));
    
    printf("Number of Failing: ");
    
